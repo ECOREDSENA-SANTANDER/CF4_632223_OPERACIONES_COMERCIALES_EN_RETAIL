839c5dca96f2d184e00d7d60ff1cdf9743b5953e
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2024-11-26 12:47:04 -0500

    7mo Avance SI

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -13,7 +13,7 @@
         .cajon.color-acento-contenido.p-4.mb-4
           p La comunicación es uno de los cuatro instrumentos básicos del mercadeo, que se clasifican tradicionalmente en publicidad, relaciones públicas, promoción de ventas y venta personal. En la actualidad se han sumado otras dos, el #[em buzz marketing] o el #[em marketing]  experiencial. Por eso, hoy se suele hablar de comunicación 360º o Comunicación Integrada de #[em Marketing] (CIM). La idea es que ayude a lograr los objetivos de marketing a través de la comunicación, buscando la eficacia en las estrategias y su respectivo plan de acción, conociendo lo mejor posible al cliente objetivo e impactándolo a través de todos los medios que sean posibles (según el músculo financiero de la marca) y con una estrategia envolvente.
       .col-lg-4.col-md-6.col-6
-        img(src='@/assets/curso/temas/', alt='')
+        //img(src='@/assets/curso/temas/', alt='')
 
 
 
